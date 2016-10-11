b9c435ec14561d9931515886fb7d8279ca585236
Marcus Hurney <marcushurney@gmail.com>
2016-10-10 22:25:45 -0400

    Initial Commit

---
+++ secret.js
@@ -0,0 +1,8 @@
+// Hold application secrets and configurations
+
+module.exports = {
+	database: 'mongodb://mjuice:abc123@ds011439.mlab.com:11439/amznclone',
+	secret: 'jdhdhd7d6d6dgdg6',
+	sendGridUser: 'marcushurney',
+	sendGridPass: 'undetermined'
+};
